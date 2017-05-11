d27065f25f71221d61a4eb451d1f786fc51e8ca7
jaasonw <arcticsmaildump@gmail.com>
2017-05-10 19:56:56 -0700

    indentation

--- InputHandler.java
+++ InputHandler.java
@@ -5,32 +5,24 @@ import javafx.scene.Scene;
 import java.util.ArrayList;
 
 public class InputHandler {
-  public static ArrayList<String> pressedKeys = new ArrayList<String>();
-
-  public static ArrayList<String> detectKeyStrokes(Scene randomScene) {
-            randomScene.setOnKeyPressed(
-                new EventHandler<KeyEvent>()
-                {
-                    public void handle(KeyEvent e)
-                    {
-                        String code = e.getCode().toString();
-              System.out.println("I DETECTED A KEY!");
-                        // only add once... prevent duplicates
-                        if ( !pressedKeys.contains(code) )
-                            pressedKeys.add( code );
-                    }
-                });
-
-            randomScene.setOnKeyReleased(
-                new EventHandler<KeyEvent>()
-                {
-                    public void handle(KeyEvent e)
-                    {
-                            System.out.println("I DETECTED A KEY!");
-                        String code = e.getCode().toString();
-                        pressedKeys.remove( code );
-                    }
-                });
-            return pressedKeys;
-    	}
+    public static ArrayList<String> pressedKeys = new ArrayList<String>();
+    public static ArrayList<String> detectKeyStrokes(Scene randomScene) {
+        randomScene.setOnKeyPressed( new EventHandler<KeyEvent>() {
+            public void handle(KeyEvent e) {
+                String code = e.getCode().toString();
+                System.out.println("I DETECTED A KEY!");
+                // only add once... prevent duplicates
+                if (!pressedKeys.contains(code))
+                pressedKeys.add(code);
+            }
+        });
+        randomScene.setOnKeyReleased(new EventHandler<KeyEvent>() {
+            public void handle(KeyEvent e) {
+                System.out.println("I DETECTED A KEY!");
+                String code = e.getCode().toString();
+                pressedKeys.remove(code);
+            }
+        });
+        return pressedKeys;
+    }
 }
